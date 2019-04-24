93b1725a2946c3208e7e301384a0efa9f85f7958
Vishal Chourey <vishalchourey@Vishals-MacBook-Air.local>
2019-04-24 14:00:04 +0530

    First commit

---
+++ bikerental/includes/footer.php
@@ -0,0 +1,49 @@
+
+
+<footer>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6">
+          
+          <ul>
+
+
+          
+           
+           
+               <li><a href="admin/">Admin Login</a></li>
+          </ul>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+         
+         
+            <form method="post">
+              <div class="form-group">
+               
+              </div>
+              
+            </form>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-push-6 text-right">
+          <div class="footer_widget">
+            
+          </div>
+        </div>
+        <div class="col-md-6 col-md-pull-6">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
